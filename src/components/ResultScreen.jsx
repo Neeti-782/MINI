4ab3fa7ae461c76f9cadff2a97119a31cce0990e
Neeti <neeti.mongoosetech@gmail.com>
2026-02-12 13:37:07 +0530

    new

--- src/components/ResultScreen.jsx
+++ src/components/ResultScreen.jsx
@@ -9,29 +9,31 @@ const ResultScreen = () => {
 
   return (
     <>
-      <div className="bg-white p-8 rounded-2xl shadow-xl text-center">
-        <h1 className="text-3xl font-bold text-pink-600 mb-4">
-          Thank u for your're Answers.
-        </h1>
-        <h2 className="text-2xl font-bold text-pink-600 mb-4">
-          You said YES {score} times 😄
-        </h2>
+      {!closeSubmit && (
+        <div className="bg-white p-8 rounded-2xl shadow-xl text-center">
+          <h1 className="text-3xl font-bold text-pink-600 mb-4">
+            Thank u for your're Answers.
+          </h1>
+          <h2 className="text-2xl font-bold text-pink-600 mb-4">
+            You said YES {score} times 😄
+          </h2>
 
-        <div className="flex items-center justify-between">
-          <button
-            onClick={() => dispatch(resetQuiz())}
-            className="bg-pink-500 text-white px-6 py-2 rounded-lg hover:bg-pink-600"
-          >
-            Restart
-          </button>
-          <button
-            onClick={() => dispatch(submitFinalQuiz())}
-            className="bg-pink-500 text-white px-6 py-2 rounded-lg hover:bg-pink-600"
-          >
-            Submit
-          </button>
+          <div className="flex items-center justify-between">
+            <button
+              onClick={() => dispatch(resetQuiz())}
+              className="bg-pink-500 text-white px-6 py-2 rounded-lg hover:bg-pink-600"
+            >
+              Restart
+            </button>
+            <button
+              onClick={() => dispatch(submitFinalQuiz())}
+              className="bg-pink-500 text-white px-6 py-2 rounded-lg hover:bg-pink-600"
+            >
+              Submit
+            </button>
+          </div>
         </div>
-      </div>
+      )}
 
       {closeSubmit && (
         <div className="bg-white p-8 rounded-2xl shadow-xl text-center">
